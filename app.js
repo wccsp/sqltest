c215f28e182644bf4ef7b59fe7e0ce4c709acc9d
wccsp <cludden@westmoreland.com>
2015-04-09 08:48:20 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var Sequelize = require('sequelize');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+var sequelize = new Sequelize('ReferenceData', 'test_account', 'Tr1pl3r41nb0w', {
+  host: '198.233.35.37',
+  dialect: 'mssql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+var Record = sequelize.define('test_table', {
+  Column_1: {
+    type: Sequelize.STRING,
+    field: 'Column_1',
+    primaryKey: true
+  }
+},{
+  freezeTableName: true,
+  timestamps: false
+});
+
+Record.findAll().then(function(records) {
+  console.log(records);
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
